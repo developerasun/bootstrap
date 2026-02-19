453a3d64029953f24f569e0450d2a0efe9f65447
developerasun <nellow1102@gmail.com>
2026-02-19 13:10:55 +0900

    add base dev sh setup

---
+++ dev.run.sh
@@ -0,0 +1,21 @@
+FLAG=$1
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color (색상 초기화)
+
+if [ -z "${FLAG}" ]; then
+  echo -e "${RED}[LOG]${NC} implement me"  
+  exit 1
+fi 
+
+case "${FLAG}" in
+  --base)
+  echo -e "${RED}[LOG]${NC} implement me"  
+  exit 1
+
+  *)
+  ;;
+esac
