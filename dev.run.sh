33886409c36749cbc203b4b518c97fb1fbd94040
developerasun <nellow1102@gmail.com>
2026-02-19 13:13:55 +0900

    add warning log

--- dev.run.sh
+++ dev.run.sh
@@ -7,15 +7,18 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color (색상 초기화)
 
 if [ -z "${FLAG}" ]; then
-  echo -e "${RED}[LOG]${NC} implement me"  
+  echo -e "${RED}[LOG]${NC} implement non flag"  
   exit 1
 fi 
 
 case "${FLAG}" in
   --base)
-  echo -e "${RED}[LOG]${NC} implement me"  
+  echo -e "${RED}[LOG]${NC} implement base flag"  
   exit 1
+  ;;
 
   *)
+  echo -e "${RED}[LOG]${NC} implement default flag"  
+  exit 1
   ;;
 esac
